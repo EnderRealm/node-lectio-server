125564abc526665afcd6e96b1d487a0fc6ac8b82
Steve Macbeth <steve_macbeth@hotmail.com>
2013-06-25 11:27:44 -0700

    moved expandUrl to netlib.js

---
+++ netlib.js
@@ -0,0 +1,23 @@
+var Q = require('q');
+var request = require('request');
+
+
+module.exports = {
+    
+    expandUrl : function(shortUrl) {
+        var deferred = Q.defer();
+        request( { method: "HEAD", url: shortUrl, followAllRedirects: true },
+            function (error, response) {
+                if (error) {
+                    deferred.reject(new Error(error));
+                } else {
+                    deferred.resolve(response.request.href);
+                }
+            });
+        return deferred.promise;
+    }
+    
+    
+    
+}
+
