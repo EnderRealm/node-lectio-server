c40d56f724aba31c96bc36c697f62798d90951bf
Steve Macbeth <steve_macbeth@hotmail.com>
2013-07-19 17:09:38 -0700

    fix access_token expiry problem with FB api

--- test.js
+++ test.js
@@ -31,129 +31,74 @@ var urls = twitter.extractUrls(test);
 
 
 
+var fb = require('fb');
+var step = require('step');
 
-
-// var fb_get_token_url = 'https://graph.facebook.com/oauth/authorize?type=user_agent&client_id=477696958990165&redirect_uri=http%3A%2F%2Fenderrealm.com&scope=read_stream,offline_access';
-
-
-
-
-// function getFbData(callback) {
-    // var options = {
-        // host: 'graph.facebook.com',
-        // port: 443,
-        // path: 'http://graph.facebook.com/oauth/authorize?type=user_agent&client_id=477696958990165&redirect_uri=http%3A%2F%2Fwww.enderrealm.com&scope=read_stream,offline_access', //apiPath example: '/me/friends'
-        // method: 'GET',
-        // followRedirect: true
-    // };
+   
+var fb_access_token = 'CAAGydn2NB1UBAGi0qfTOxa1nHslIk5uOXG26ye2Y20ZBrk7UuxJoudvR6QYtwdb8f51351bOuuvmuEa2y791hOsnZAx4W1eKBB7P81YCHsRWwmLpdZBns4UwZB5Ye6ICFsi4RJBlEgnatdhrQ7M1TDvERFPtFZAQZD';
 
 
 
 
-    // request('http://graph.facebook.com/oauth/authorize?type=user_agent&client_id=477696958990165&redirect_uri=http%3A%2F%2Fwww.enderrealm.com&scope=read_stream,offline_access', function(error, response, body) {
+function fbExtendAccessToken(fb, options, callback) {
     
-        // console.log(response);
-        // console.log(body);
-    
-    // });
-
-
-    var fb = require('fb');
-    var step = require('step');
-    
-    
-    //https://github.com/Thuzi/facebook-node-sdk#get-facebook-application-access-token
-    
-    
-    fb.api('oauth/access_token', { client_id: '477696958990165', client_secret: '67a0826bb847ff02c14fa6db6276aa61', redirect_uri: 'http://www.enderrealm.com', grant_type: 'fb_exchange_token', fb_exchange_token:'CAAGydn2NB1UBAMopsEVebMV626ZCiHYMcwS03JMGMMHO3fxG7Dik0OlT5tKiwZCwNySjAl0WDcprS3TVTsNyZAufIUZASHq3I32Wzev9pkgNmXZBoc311O5ZCM0dyHL9b7EcAIYMgKnG8ikiz5jWfL'}, function(res) {
+    fb.api('oauth/access_token', options, function(res) {
     
         if(!res || res.error) {
         
-            console.log(!res ? 'error occured' : res.error);        
-            return;
+            console.log(!res ? 'error occured' : res.error);
+            callback(res.error);
         }
     
-        var accessToken = res.access_token;
+        callback(null, res);
         
-        fb.setAccessToken(accessToken);
-
-        util.debug(res, 1); 
-
-        
-
-        fb.api('me/home', {fields: ['type']}, function(res) {
-
-            if(!res || res.error) {
-            
-                console.log(!res ? 'error occured' : res.error);
-                return;
-            
-            }
-
-            util.debug(res, 1); 
-
-            console.log(res.data);
-
-        });
-
-
-
-    
     });
     
+}    
+
     
+
+console.log(fb_access_token);
+
+fb.setAccessToken(fb_access_token);
+
+fb.api('fql', {q:"SELECT post_id, actor_id, target_id, message, attachment FROM stream"}, function lambda(res) {
+
+    if(!res || res.error) {
     
-  
-    
-    
-    
-    
-    
-    // var buffer = ''; //this buffer will be populated with the chunks of the data received from facebook
-    // var req = request.get(options, function(result){
+        if(res.error.type = 'OAuthException') {
         
-        // util.debug(request,20);
-        
-        // result.setEncoding('utf8');
-        // result.on('data', function(chunk){
-            // buffer += chunk;
-        // });
+            fbExtendAccessToken(fb, { client_id: '477696958990165', client_secret: '67a0826bb847ff02c14fa6db6276aa61', redirect_uri: 'http://www.enderrealm.com', grant_type: 'fb_exchange_token', fb_exchange_token:'CAAGydn2NB1UBAMopsEVebMV626ZCiHYMcwS03JMGMMHO3fxG7Dik0OlT5tKiwZCwNySjAl0WDcprS3TVTsNyZAufIUZASHq3I32Wzev9pkgNmXZBoc311O5ZCM0dyHL9b7EcAIYMgKnG8ikiz5jWfL'}, function(err,res){
 
-        // result.on('end', function(){
-            // callback(buffer);
-        // });
-    // });
+                var accessToken = res.access_token;
+                var expiry = res.expires;
+    
+                fb.setAccessToken(accessToken);
+    
+                console.log('expiry = '+expiry);
+    
+                // check if access token expires in less then three days (60*60*24*3)
+                if(expiry < (259200)) {
+                    console.log('token expires in less then three days : '+expiry);
+                }
 
-    // req.on('error', function(e){
-        // util.logger(util.ERROR, 'error from facebook.getFbData: ' + e.message);
-    // });
+                fb.api('me/home', lambda);
+                
+            });
+        }
+    
+        console.log(!res ? 'error occured' : res.error);
+        return;
+    
+    }
 
-    // req.end();
-// }
+    util.debug(res, 1); 
 
-
-
-// getFbData(function(data) {
-    // console.log(data);
-
-// });
-
-
-
-// var test = 'test';
+});
 
 
-// console.log(test || '');
-
-
-
-
-            // var created = new Date();
-            
-            // console.log('link -- ' + created.getMonth()+'/'+created.getDate()+' '+created.getHours()+':'+created.getMinutes());
-            
-            
-            
-            // created = new Date();
-            
-            // console.log(created.getTime());
+    
+    
+ 
+    
+    
